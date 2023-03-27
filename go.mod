01c7fb9fb634952eaae6d0cfa59fe7055a5b4b2b
Trym123 <trymlundby@gmail.com>
2023-03-27 12:49:10 +0000

    Laget scanner fil

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Trym123/minyr
 
 go 1.18
+
+require github.com/Trym123/funtemps v0.0.0-20230218155934-dd7d26385d85
--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
-import {
+import (
 	"os"
 	"log"
 	"io"
 	"strings"
-	"github.com/Trym123/funtemps"
-}
+	"github.com/Trym123/funtemps/conv"
+)
 
 func main() {
-	src, err := os.Open("/home/TrymA/minyr/kjevik-temp-celsius-20220318-20230318.csv")
+	src, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer src.Close()
+	dest, err := OpenFile("kjevik-temp-fahr-20220318-20230318.csv", os.O_RDWR|os.O_CREATE, 0644)
 	log.Println(src)
+	
 
 	var buffer []byte
 	var linebuf []byte //nil
@@ -22,24 +24,23 @@ func main() {
 	bytesCount := 0
 
 	for {
-	
-	_, err := src.Read(buffer)
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
-	}
+		_, err := src.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
 
-	bytesCount++
-	if buffer[0] == 0x0A {
-		log.Println(string(linebuf))
-		elementArray := strings.Split(string(linebuf), ",")
+		bytesCount++
+		if buffer[0] == 0x0A {
+			log.Println(string(linebuf))
+			elementArray := strings.Split(string(linebuf), ",")
 		if len(elementArray) > 3 {
 			celsius := elementArray[3]
 			fahr := conv.CelsiusToFahrenheit(celsius)
 			log.Println(elementArray[3])
 		}
 		linebuf = nil
-	} else {
-		linebuf = append(linebuf, buffer[0])
+		} else {
+			linebuf = append(linebuf, buffer[0])
 		}
 		if err == io.EOF {
 			break
